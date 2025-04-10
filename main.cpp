c5e5fee078c752cc401401bfb78220750c20a332
pablods2 <pablo.dominguez2@alu.uclm.es>
2025-04-10 12:08:44 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // Librería para manejar entradas y salidas
+
+using namespace std;  // Evita tener que escribir "std::" antes de cada comando de la librería estándar
+
+int main() {
+    // Imprime un mensaje en la consola
+    cout << "¡Hola, Mundo!" << endl;
+
+    return 0;  // Devuelve 0 para indicar que el programa terminó correctamente
+}
